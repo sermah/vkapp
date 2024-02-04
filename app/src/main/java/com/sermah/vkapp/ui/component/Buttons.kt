873dcf34cdc468ba999c6ac3ea306c1a924594e9
sermah <sermahus@gmail.com>
2024-02-04 19:17:35 +0300

    Smooth button color animation

--- app/src/main/java/com/sermah/vkapp/ui/component/Buttons.kt
+++ app/src/main/java/com/sermah/vkapp/ui/component/Buttons.kt
@@ -1,15 +1,22 @@
 package com.sermah.vkapp.ui.component
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Check
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sermah.vkapp.ui.theme.AppType
 import com.sermah.vkapp.ui.utils.displayCount
@@ -45,19 +52,59 @@ fun VKPost_Button(
         end = 14.dp,
         bottom = 4.dp
     )
-    if (toggle)
-        Button(
-            onClick = onClick,
-            modifier = modifier,
-            contentPadding = paddingValues,
-            content = content,
-        )
-    else
-        OutlinedButton(
-            onClick = onClick,
-            modifier = modifier,
-            contentPadding = paddingValues,
-            content = content,
-        )
+    val animatedContainerColor = animateColorAsState(
+        targetValue = if (toggle)
+            MaterialTheme.colorScheme.primary
+        else MaterialTheme.colorScheme.primaryContainer,
+        animationSpec = tween(COLOR_TWEEN_DURATION_MS, 0, LinearEasing),
+        label = "",
+    )
+    val animatedContentColor = animateColorAsState(
+        targetValue = if (toggle)
+            MaterialTheme.colorScheme.onPrimary
+        else MaterialTheme.colorScheme.onPrimaryContainer,
+        animationSpec = tween(COLOR_TWEEN_DURATION_MS, 0, LinearEasing),
+        label = "",
+    )
+
+    Button(
+        colors = ButtonDefaults.buttonColors(
+            containerColor = animatedContainerColor.value,
+            contentColor = animatedContentColor.value,
+        ),
+        onClick = onClick,
+        modifier = modifier,
+        contentPadding = paddingValues,
+        content = content,
+    )
 }
 
+private const val COLOR_TWEEN_DURATION_MS = 100
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFE0E0E0,
+)
+@Composable
+fun VKPost_ButttonPreview_on() {
+    VKPost_Button(
+        icon = Icons.Outlined.Check,
+        count = 1234,
+        toggle = true,
+        onClick = {},
+    )
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFE0E0E0,
+)
+@Composable
+fun VKPost_ButttonPreview_off() {
+    VKPost_Button(
+        icon = Icons.Outlined.Check,
+        count = 1234,
+        toggle = false,
+        onClick = {},
+    )
+}
